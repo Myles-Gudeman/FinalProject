4e09ed1240ef6323979dabe78af9a3e2a5c3bca6
abloblo1 <andrew739@gmail.com>
2018-12-05 05:49:34 -0600

    game part works

--- app/src/main/java/com/example/andrew/mp6/MainActivity2.java
+++ app/src/main/java/com/example/andrew/mp6/MainActivity2.java
@@ -3,47 +3,108 @@ package com.example.andrew.mp6;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity2 extends AppCompatActivity {
-    private Button higherButton;
-    private Button lowerButton;
-    private Button menuButton1;
 
+    private Button start;
+    private Button lower;
+    private Button higher;
+    private Button mainMenu;
 
+    private TextView currentScore;
+    private TextView invisible;
+
+    private Random random1;
+
+    private int displayNumber;
+    private int numberNew;
+    private int score = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gameplay_screen);
 
-        menuButton1 = findViewById(R.id.Main_Menu);
-        lowerButton = findViewById(R.id.Lower);
-        higherButton = findViewById(R.id.Higher);
+        start = findViewById(R.id.Start);
+        lower = findViewById(R.id.lower);
+        higher = findViewById(R.id.higher);
+        mainMenu = findViewById(R.id.Main_Menu);
 
-        menuButton1.setOnClickListener(new View.OnClickListener() {
+        start.setVisibility(View.VISIBLE);
+        lower.setVisibility(View.INVISIBLE);
+        higher.setVisibility(View.INVISIBLE);
+        mainMenu.setVisibility(View.VISIBLE);
+
+
+        currentScore = findViewById(R.id.currentScore);
+        invisible = findViewById(R.id.invisibleNumber);
+
+        random1 = new Random();
+
+        start.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                openHome_Screen();
+            public void onClick(View view) {
+                displayNumber = random1.nextInt(100) + 1;
+                invisible.setText(("" + displayNumber));
+                score = 0;
+                currentScore.setText(("Score: " + score));
+
+                start.setVisibility(View.INVISIBLE);
+                lower.setVisibility(View.VISIBLE);
+                higher.setVisibility(View.VISIBLE);
             }
         });
 
-        lowerButton.setOnClickListener(new View.OnClickListener() {
+        lower.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                openHome_Screen();
+            public void onClick(View view) {
+                numberNew = random1.nextInt(100) + 1;
+                invisible.setText(("" + numberNew));
+                if (numberNew <= displayNumber) {
+                    score++;
+                    currentScore.setText(("Score: " + score));
+                    displayNumber = numberNew;
+                }
+                else {
+                    currentScore.setText(("Score: " + score + "\nGame Over!"));
+                    start.setVisibility(View.VISIBLE);
+                    lower.setVisibility(View.INVISIBLE);
+                    higher.setVisibility(View.INVISIBLE);
+                }
             }
         });
 
-        higherButton.setOnClickListener(new View.OnClickListener() {
+        higher.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                openHome_Screen();
+            public void onClick(View view) {
+                numberNew = random1.nextInt(100) + 1;
+                invisible.setText(("" + numberNew));
+                if (numberNew >= displayNumber) {
+                    score++;
+                    currentScore.setText(("Score: " + score));
+                    displayNumber = numberNew;
+                }
+                else {
+                    currentScore.setText(("Score: " + score + "\nGame Over!"));
+                    start.setVisibility(View.VISIBLE);
+                    lower.setVisibility(View.INVISIBLE);
+                    higher.setVisibility(View.INVISIBLE);
+                }
             }
         });
 
+        mainMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHome_Screen();
+            }
+        });
     }
-
     public void openHome_Screen() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
