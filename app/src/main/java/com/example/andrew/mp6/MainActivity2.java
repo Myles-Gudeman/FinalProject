818e92c5918694b0d2793a26d6787b9ed17d18cd
abloblo1 <andrew739@gmail.com>
2018-12-08 23:02:10 -0600

    everything done

--- app/src/main/java/com/example/andrew/mp6/MainActivity2.java
+++ app/src/main/java/com/example/andrew/mp6/MainActivity2.java
@@ -1,12 +1,18 @@
 package com.example.andrew.mp6;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
 import java.util.Random;
 
@@ -94,6 +100,7 @@ public class MainActivity2 extends AppCompatActivity {
                     start.setVisibility(View.VISIBLE);
                     lower.setVisibility(View.INVISIBLE);
                     higher.setVisibility(View.INVISIBLE);
+                    saveScore(score);
                 }
             }
         });
@@ -108,5 +115,28 @@ public class MainActivity2 extends AppCompatActivity {
     public void openHome_Screen() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+    public void saveScore(int score) {
+        SharedPreferences scorePref = getSharedPreferences("userScore", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = scorePref.edit();
+        int lowestHighScore = (scorePref.getInt("fifth", 0));
+        if (score > lowestHighScore) {
+            //adding new highScore
+            List<Integer> scoreList = new ArrayList<>();
+            scoreList.add(scorePref.getInt("first", 0));
+            scoreList.add(scorePref.getInt("second", 0));
+            scoreList.add(scorePref.getInt("third", 0));
+            scoreList.add(scorePref.getInt("fourth", 0));
+            scoreList.add(scorePref.getInt("fifth", 0));
+            scoreList.add(score);
+            Collections.sort(scoreList, Collections.<Integer>reverseOrder());
+            editor.putInt("first", scoreList.get(0));
+            editor.putInt("second", scoreList.get(1));
+            editor.putInt("third", scoreList.get(2));
+            editor.putInt("fourth", scoreList.get(3));
+            editor.putInt("fifth", scoreList.get(4));
+            editor.apply();
+        }
+
     }
 }
--- app/src/main/java/com/example/andrew/mp6/MainActivity3.java
+++ app/src/main/java/com/example/andrew/mp6/MainActivity3.java
@@ -1,10 +1,13 @@
 package com.example.andrew.mp6;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
     private Button menuButton2;
@@ -22,6 +25,23 @@ public class MainActivity3 extends AppCompatActivity {
                 openHome_Screen2();
             }
         });
+
+        TextView score = (TextView) findViewById(R.id.score);
+        TextView score1 = (TextView) findViewById(R.id.score1);
+        TextView score2 = (TextView) findViewById(R.id.score2);
+        TextView score3 = (TextView) findViewById(R.id.score3);
+        TextView score4 = (TextView) findViewById(R.id.score4);
+        SharedPreferences scorePref = getSharedPreferences("userScore", Context.MODE_PRIVATE);
+        int board = scorePref.getInt("first", 0);
+        int board1 = scorePref.getInt("second", 0);
+        int board2 = scorePref.getInt("third", 0);
+        int board3 = scorePref.getInt("fourth", 0);
+        int board4 = scorePref.getInt("fifth", 0);
+        score.setText(("1st Best:  " + board));
+        score1.setText(("2nd Best:  " + board1));
+        score2.setText(("3rd Best:  " + board2));
+        score3.setText(("4th Best:  " + board3));
+        score4.setText(("5th Best:  " + board4));
 
     }
 
